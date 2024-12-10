38b8e3348a3457eabae75d836db8326721837112
Kirill <kirillbk@gmail.com>
2024-12-10 09:24:06 +0300

    10

---
+++ 2024/10.py
@@ -0,0 +1,62 @@
+# --- Day 10: Hoof It ---
+
+from sys import stdin
+
+
+def solution1(puzzle: list[list[int]]) -> int:
+    def _dfs(x: int, y: int):
+        if puzzle[y][x] == 0:
+            nonlocal ans
+            ans += 1
+        visited.add((x, y))
+        steps = (0, 1), (0, -1), (1, 0), (-1, 0)
+        for dx, dy in steps:
+            to_x = x + dx
+            to_y = y + dy
+            if (
+                0 <= to_x < len(puzzle[0])
+                and 0 <= to_y < len(puzzle)
+                and (to_x, to_y) not in visited
+                and puzzle[to_y][to_x] == puzzle[y][x] - 1
+            ):
+                _dfs(to_x, to_y)
+
+    ans = 0
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[0])):
+            if puzzle[i][j] == 9:
+                visited = set()
+                _dfs(j, i)
+
+    return ans
+
+
+def solution2(puzzle: list[list[int]]) -> int:
+    def _dfs(x: int, y: int):
+        if puzzle[y][x] == 0:
+            nonlocal ans
+            ans += 1
+        steps = (0, 1), (0, -1), (1, 0), (-1, 0)
+        for dx, dy in steps:
+            to_x = x + dx
+            to_y = y + dy
+            if (
+                0 <= to_x < len(puzzle[0])
+                and 0 <= to_y < len(puzzle)
+                and puzzle[to_y][to_x] == puzzle[y][x] - 1
+            ):
+                _dfs(to_x, to_y)
+
+    ans = 0
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[0])):
+            if puzzle[i][j] == 9:
+                _dfs(j, i)
+
+    return ans
+
+
+puzzle = [list(map(int, line.rstrip())) for line in stdin]
+
+print(solution1(puzzle))
+print(solution2(puzzle))
